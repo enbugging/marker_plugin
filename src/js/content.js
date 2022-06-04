e022dce8b873f70073852a8290a5d4647d51dd9b
Nguyễn Doãn Đại <40011497+MegumiTadokoro@users.noreply.github.com>
2022-06-04 10:50:11 +0200

    Make prototype of plugin, without the highlight
    Co-authored-by: Nguyễn Tuấn Dũng <ntddebugger@gmail.com>

--- src/js/content.js
+++ src/js/content.js
@@ -1,22 +1,79 @@
 import highlight from "./marker/text_processing.js";
 
-export function main() {
-    var elements = document.getElementsByTagName("*");
+const highlightAttr = "highlighted----";
 
-    for (var element of elements) {
-        if (element.innerText == null) continue;
-        console.log(element.innerText);
-        continue;
-        for (var node of element.childNodes) {
-            if (node.nodeType === 3 && /[a-zA-Z0-9]/.test(node.nodeValue)) {
-                //var text = node.nodeValue;
-                console.log(node.innerText);
-                //var boldness = highlight(text);
-                //newDiv = document.createElement("span");
-                //newDiv.appendChild(document.createTextNode(text));
-                //newDiv.style.color = "black";
-                //element.replaceChild(newDiv, node);
+function textNodesUnder(el) {
+    var n,
+        a = [],
+        walk = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null);
+    while ((n = walk.nextNode())) a.push(n);
+    return a;
+}
+
+function extractToDiv(node) {
+    const g = document.createElement("span");
+    g.setAttribute("class", highlightAttr);
+    node.nodeValue
+        .split("")
+        .map((c) => {
+            const span = document.createElement("span");
+            span.setAttribute("class", highlightAttr);
+            span.setAttribute("style", "color: aquamarine;");
+            span.appendChild(document.createTextNode(c));
+            return span;
+        })
+        .forEach((span) => g.appendChild(span));
+    return g;
+}
+
+function applyHighlight(doc) {
+    const elements = Array.from(textNodesUnder(doc));
+    const divElements = elements
+        .filter(
+            (node) =>
+                node.nodeValue &&
+                /[^\s\n]+/.test(node.nodeValue) &&
+                node.parentElement.tagName !== "SCRIPT" &&
+                node.parentElement.tagName !== "STYLE"
+        )
+        .map((node) => ({
+            node,
+            newDiv: extractToDiv(node),
+        }));
+
+    divElements.forEach(({ node, newDiv }) => {
+        if (node.parentNode) node.parentNode.replaceChild(newDiv, node);
+    });
+}
+
+export function main() {
+    applyHighlight(document.body);
+    // Callback function to execute when mutations are observed
+    const callback = function (mutationsList, observer) {
+        // Use traditional 'for loops' for IE 11
+        for (const mutation of mutationsList) {
+            if (mutation.type === "childList") {
+                mutation.addNodes
+                    .filter(
+                        (node) =>
+                            node.classList !== undefined &&
+                            !node.classList.contains(highlightAttr)
+                    )
+                    .map((x) => {
+                        console.log("Updating", mutation.target);
+                        return x;
+                    })
+                    .forEach(applyHighlight);
             }
+            // mutation of us -> continue
         }
-    }
+    };
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+    // Start observing the target node for configured mutations
+    observer.observe(document, {
+        childList: true,
+        subtree: true,
+    });
 }
